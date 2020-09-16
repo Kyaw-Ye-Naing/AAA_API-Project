0722bb0bb87745b5513f0e288bca5cb46fd837f2
Kyaw Ye Naing <kyawyenaing771619@gmail.com>
2020-09-16 16:03:04 +0630

    Login api

--- AAA_API/Controllers/HomeController.cs
+++ AAA_API/Controllers/HomeController.cs
@@ -4,23 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using AAA_API.Models;
 using Hangfire;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
 
 namespace AAA_API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly Gambling_AppContext db;
-        public HomeController(ILogger<HomeController> logger, Gambling_AppContext _db)
+        private readonly Gambling_AppContext _context;
+        //private IUserService userService;
+        public HomeController(ILogger<HomeController> logger, Gambling_AppContext context)
         {
             _logger = logger;
-            db = _db;
+            _context = context;
         }
 
 
@@ -28,9 +32,41 @@ namespace AAA_API.Controllers
         public IEnumerable<TblLeague> Index()
         {
             
-            return db.TblLeague.ToList();
+            return _context.TblLeague.ToList();
         }
 
+        //LogIn 
+        [AllowAnonymous]
+        [Route("authenicate")]
+        [HttpPost]
+        public IActionResult LogIn(AuthenicateModel user)
+        {
+            TblUser tblUser = new TblUser();
+            //Check username and password are empty
+            if (string.IsNullOrEmpty(user.username) || string.IsNullOrEmpty(user.password)) 
+            {
+                return BadRequest(new { status = "Usename or password is empty" });
+            }
+
+            //Get all information of logged user
+             var userValue = _context.TblUser.SingleOrDefault(x => x.Username == user.username && x.Password==user.password && x.Lock==false);
+
+            //Check username and password are incorrect
+           if (userValue == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+           //Check logged user account is locked
+           var Userlock= _context.TblUser.Where(a => a.Username.Equals(user.username)).FirstOrDefault().Lock;
+            if (tblUser.Lock == false)
+            {
+                return BadRequest(new { message = "Your account is lock!" });
+            }
+
+            //return logged user information
+            return Ok(userValue);
+        }
         
 
        
