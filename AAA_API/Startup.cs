2194ce54a478fa7a52e9a2f686ca417f3d2d88b1
Kyaw Ye Naing <kyawyenaing771619@gmail.com>
2020-09-16 10:20:31 +0630

    Remove Bg Task

--- AAA_API/Startup.cs
+++ AAA_API/Startup.cs
@@ -42,7 +42,7 @@ namespace AAA_API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, 
+        public void Configure(IApplicationBuilder app,
             IWebHostEnvironment env,
              IBackgroundJobClient backgroundJobClient,
             IRecurringJobManager recurringJobManager,
